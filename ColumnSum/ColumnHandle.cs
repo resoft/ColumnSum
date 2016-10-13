75d382c284a1566868506aec548cfb1abb048fd6
LOVE <LOVE@LOVE-HP>
2016-10-13 10:43:28 +0800

    依照建議修改

--- ColumnSum/ColumnHandle.cs
+++ ColumnSum/ColumnHandle.cs
@@ -7,67 +7,47 @@ using System.Threading.Tasks;
 
 namespace ColumnSum
 {
-    public class ColumnHandlet<T>
+    public static class ColumnHandlet
     {
-
-        public List<int> SumList(int rowSum)
+        public static IEnumerable<int> GetSum<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector, int rowSum)
         {
-            IEnumerable<T> columnData = GetColumnData();
-            int number;
-            var returnList = columnData.Select((v, i) => new { val = v, index = i + 1 })
-                  .Where(x => int.TryParse(x.val.ToString(), out number))
-                  .GroupBy(x => Convert.ToInt16(Math.Ceiling((double)x.index / rowSum)))
-                  .Select(x =>
-                  new
-                  {
-                      Index = x.Key,
-                      SumValue = x.Sum(w => int.Parse(w.val.ToString()))
-                  });
-
-            return returnList.Select(x => x.SumValue).ToList<int>();
+            int index = 0;
+            while (index < source.Count())
+            {
+                yield return source.Skip(index).Take(rowSum).Sum(selector);
+                index += rowSum;
+            }
         }
 
-        private IEnumerable<T> _data;
+        //public List<int> SumList(int rowSum)
+        //{
+        //    IEnumerable<T> columnData = GetColumnData();
+        //    int number;
+        //    var returnList = columnData.Select((v, i) => new { val = v, index = i + 1 })
+        //          .Where(x => int.TryParse(x.val.ToString(), out number))
+        //          .GroupBy(x => Convert.ToInt16(Math.Ceiling((double)x.index / rowSum)))
+        //          .Select(x =>
+        //          new
+        //          {
+        //              Index = x.Key,
+        //              SumValue = x.Sum(w => int.Parse(w.val.ToString()))
+        //          });
 
-        public void SetData(IEnumerable<T> data)
-        {
-            this._data = data;
-        }
+        //    return returnList.Select(x => x.SumValue).ToList<int>();
+        //}
 
-        protected virtual IEnumerable<T> GetColumnData()
-        {
-            return this._data;
-        }
-    }
+        //private IEnumerable<T> _data;
 
-    public class Table
-    {
-        public List<Order> Get()
-        {
-            List<Order> orderList = new List<Order>();
-            orderList.Add(new Order() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21, Other = 1 });
-            orderList.Add(new Order() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22, Other = 1 });
-            orderList.Add(new Order() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23, Other = 1 });
-            orderList.Add(new Order() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24, Other = "" });
-            orderList.Add(new Order() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25, Other = 1 });
-            orderList.Add(new Order() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26, Other = "test" });
-            orderList.Add(new Order() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27, Other = 1 });
-            orderList.Add(new Order() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28, Other = 1 });
-            orderList.Add(new Order() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29, Other = 0 });
-            orderList.Add(new Order() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30, Other = 1 });
-            orderList.Add(new Order() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31, Other = 1 });
+        //public void SetData(IEnumerable<T> data)
+        //{
+        //    this._data = data;
+        //}
 
-            return orderList;
-        }
-    }
+        //protected virtual IEnumerable<T> GetColumnData()
+        //{
+        //    return this._data;
+        //}
 
-    public class Order
-    {
-        public int Id { get; set; }
-        public int Cost { get; set; }
-        public int Revenue { get; set; }
-        public int SellPrice { get; set; }
 
-        public object Other { get; set; }
     }
 }
---
+++ ColumnSumTest/OrdersTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ColumnSum;
+using System.Collections.Generic;
+using ExpectedObjects;
+using System.Linq;
+
+namespace ColumnSumTest
+{
+    [TestClass]
+    public class OrdersTest
+    {
+        [TestMethod]
+        public void Orders_Of_3_Row_Sum_By_Cost_Should_Be_6_15_24_21()
+        {
+            var orders = new Orders().Get();
+            List<int> expected = new List<int> { 6, 15, 24, 21 };
+            List<int> actual = orders.GetSum(x => x.Cost, 3).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+            //expected.ToExpectedObject().ShouldEqual(actual);
+            //Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Orders_Of_4_Row_Sum_By_Revenue_Should_Be_50_66_60()
+        {
+            var orders = new Orders().Get();
+            List<int> expected = new List<int> { 50, 66, 60 };
+            List<int> actual = orders.GetSum(x => x.Revenue, 4).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+            //expected.ToExpectedObject().ShouldEqual(actual);
+            //Assert.AreEqual(expected, actual);
+        }
+
+    }
+
+
+    public class Orders
+    {
+
+        public IEnumerable<Order> Get()
+        {
+            List<Order> orderList = new List<Order>() {
+                new Order() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21, Other = 1 },
+                new Order() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22, Other = 1 },
+                new Order() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23, Other = 1 },
+                new Order() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24, Other = "" },
+                new Order() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25, Other = 1},
+                new Order() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26, Other = "test" },
+                new Order() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27, Other = 1 },
+                new Order() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28, Other = 1 },
+                new Order() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29, Other = 0 },
+                new Order() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30, Other = 1 },
+                new Order() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31, Other = 1 }
+        };
+
+
+            return orderList;
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
+        public object Other { get; set; }
+    }
+}
